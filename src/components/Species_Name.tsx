39efb5414b9ab12f5c2a9e8de50df2218a847c83
mousumi-ish <baral_mousumi@yahoo.com>
2023-09-09 12:38:19 +0100

    feat : created species and planet files

---
+++ src/components/Species_Name.tsx
@@ -0,0 +1,23 @@
+interface SpeciesNameProps {
+  speciesName: string;
+  onChangeSpeciesName: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+const SpeciesName: React.FC<{
+  speciesName: string;
+  onChangeSpeciesName: any;
+}> = ({ speciesName, onChangeSpeciesName }) => (
+  <>
+    <div className="container">
+      <label htmlFor="speciesName">Species Name : </label>
+      <input
+        type="text"
+        id="speciesName"
+        className="speciesName"
+        value={speciesName}
+        onChange={onChangeSpeciesName}
+      />
+    </div>
+  </>
+);
+
+export default SpeciesName;
--- src/components/W12MForm.tsx
+++ src/components/W12MForm.tsx
@@ -1,13 +1,28 @@
-import { useState } from 'react';
-import W12MHeader from './W12MHeader';
-
+import { useState } from "react";
+import W12MHeader from "./W12MHeader";
+import SpeciesName from "./Species_Name";
+import PlanetName from "./planet_name";
+const [speciesName, setSpeciesName] = useState("humans");
 const W12MForm = () => {
-	return (
-		<section className='w12MForm'>
-			<W12MHeader />
-			{/* REST OF FORM GOES HERE */}
-		</section>
-	);
+  return (
+    <section className="w12MForm">
+      <W12MHeader />
+      {/* REST OF FORM GOES HERE */}
+
+      <form>
+        <SpeciesName
+          speciesName={SpeciesName}
+          onChangeSpeciesName={(e: any) => setSpeciesName(e.target.value)}
+        />
+
+        <div className="container">
+          <button type="button" className="btn" id="btn" onClick={handleSubmit}>
+            Submit
+          </button>
+        </div>
+      </form>
+    </section>
+  );
 };
 
 export default W12MForm;
